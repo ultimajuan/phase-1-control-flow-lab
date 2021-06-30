ceb901d8e986c3038a7f9e78a8719dc308b0c278
Juan <juancollazonyc@gmail.com>
2021-06-30 09:44:30 -0400

    commit tree ee9f2681f31b4be4fec25035e7326285f0e248f5

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let ride = 300
+  if (ride <= 400) {
+    return "This one is on me!"
+  } else if (ride > 2000) {
+    return "I will gladly take your thirty bucks."
+  } 
 }
-
 function ternaryCheckCity(){
   // Write your code here!
+  let city = "NYC"
+  city ? "Ok, sounds good." : "No go."
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+
+} 
